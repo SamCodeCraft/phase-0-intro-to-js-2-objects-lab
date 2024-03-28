f65d2a91cae1ecaafd374666abef428ef7ee887b
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-28 22:35:58 +0300

    Created Object lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+let employee = {
+    name: "Siyajari Samuel",
+    streetAddress: " 567 Dog unit St"
+};
+// function to update employee with a new key-value pair(non-destructive)
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+// function to destructively update employee with new key-value pair
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+// function to delete a key-value pair from emeployee (non-destructive)
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+// function to destructively delete a key-value pair from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
